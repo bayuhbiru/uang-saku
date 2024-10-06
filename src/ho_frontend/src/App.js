9df349ee7f40a0721306bda0a178c80980b97e3d
Silkyroad <174839608+silkysilks@users.noreply.github.com>
2024-10-07 04:37:07 +0700

    Update App.js

--- src/ho_frontend/src/App.js
+++ src/ho_frontend/src/App.js
@@ -1,40 +1,24 @@
-import { html, render } from 'lit-html';
-import { ho_backend } from 'declarations/ho_backend';
-import logo from './logo2.svg';
-
-class App {
-  greeting = '';
-
-  constructor() {
-    this.#render();
-  }
-
-  #handleSubmit = async (e) => {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    this.greeting = await ho_backend.greet(name);
-    this.#render();
-  };
-
-  #render() {
-    let body = html`
-      <main>
-        <img src="${logo}" alt="DFINITY logo" />
-        <br />
-        <br />
-        <form action="#">
-          <label for="name">Enter your name: &nbsp;</label>
-          <input id="name" alt="Name" type="text" />
-          <button type="submit">Click Me!</button>
-        </form>
-        <section id="greeting">${this.greeting}</section>
-      </main>
-    `;
-    render(body, document.getElementById('root'));
-    document
-      .querySelector('form')
-      .addEventListener('submit', this.#handleSubmit);
-  }
-}
-
-export default App;
+export default {
+    data() {
+        return {
+            balance: 0,
+            message: "Selamat datang di dompet ICP!",
+        };
+    },
+    async mounted() {
+        this.balance = await this.getBalance();
+    },
+    methods: {
+        async getBalance() {
+            const response = await fetch("https://<backend-url>/getBalance");
+            const balance = await response.json();
+            return balance;
+        },
+    },
+    template: `
+        <div>
+            <h1>{{ message }}</h1>
+            <p>Saldo Anda: {{ balance }} ICP</p>
+        </div>
+    `,
+};
